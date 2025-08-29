8e6c3999319a621897ce3e176d13ebdde0396380
Brian Ali <gamerfunky78@gmail.com>
2025-08-29 10:56:59 +0300

    Update index.py

--- index.py
+++ index.py
@@ -13,3 +13,14 @@ class Superhero:
 
     def get_origin(self):
         return self._origin
+
+
+# inheritance
+class FlyingSuperhero(Superhero):
+    def __init__(self, name, power, origin, flight_speed):
+        super().__init__(name, power, origin)
+        self.flight_speed = flight_speed
+
+    def use_power(self):
+        # Polymorphism in action
+        print(f"{self.name} soars through the sky at {self.flight_speed} mph using {self.power}!")
